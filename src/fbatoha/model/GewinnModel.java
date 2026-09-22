02ebf7b10f5b9b85e6e83a08b78ddcde0b694e42
Felix <ahfgfhdjs>
2026-09-22 15:03:45 +0200

    Erstellen der Methoden

--- src/fbatoha/model/GewinnModel.java
+++ src/fbatoha/model/GewinnModel.java
@@ -1,5 +1,40 @@
 package fbatoha.model;
 
 public class GewinnModel {
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
 
+    public GewinnModel() {
+
+    }
+
+    public void berechneComputerZahl() {
+
+    }
+
+    public void berechneRunde(int spielerZahl) {
+
+    }
+
+    public boolean hatGewonnen() {
+
+    }
+
+    public boolean hatVerloren() {
+
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
 }
