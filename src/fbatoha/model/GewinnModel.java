4f25193f66fe1f464df0942c1c8b55d06e409387
Felix <ahfgfhdjs>
2026-09-23 08:19:40 +0200

    Methode "BerechneRunde" geschrieben

--- src/fbatoha/model/GewinnModel.java
+++ src/fbatoha/model/GewinnModel.java
@@ -1,5 +1,7 @@
 package fbatoha.model;
 
+import java.util.Random;
+
 public class GewinnModel {
     private int gesamtPunkte;
     private int spielerZahl;
@@ -11,19 +13,28 @@ public class GewinnModel {
     }
 
     public void berechneComputerZahl() {
-
+        computerZahl = new Random().nextInt(1,10);
     }
 
     public void berechneRunde(int spielerZahl) {
-
+        this.spielerZahl = spielerZahl;
+        berechneComputerZahl();
+        if (this.spielerZahl == this.computerZahl) {
+            this.rundenErgebnis = 20;
+        } else if (this.spielerZahl == this.computerZahl + 1 || this.spielerZahl == this.computerZahl - 1) {
+            this.rundenErgebnis = 5;
+        } else {
+            this.rundenErgebnis = -10;
+        }
+        this.gesamtPunkte += this.rundenErgebnis;
     }
 
     public boolean hatGewonnen() {
-
+        return gesamtPunkte >= 100;
     }
 
     public boolean hatVerloren() {
-
+        return gesamtPunkte <= 0;
     }
 
     public int getGesamtPunkte() {
