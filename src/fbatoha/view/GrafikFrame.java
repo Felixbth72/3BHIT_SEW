6c97505234d4d4d8ec80cd137416c0adde972c94
Felix <ahfgfhdjs>
2026-09-24 13:52:04 +0200

    Erstellung des Construktors

--- src/fbatoha/view/GrafikFrame.java
+++ src/fbatoha/view/GrafikFrame.java
@@ -3,5 +3,11 @@ package fbatoha.view;
 import javax.swing.*;
 
 public class GrafikFrame extends JFrame {
-
+    public GrafikFrame() {
+        setTitle("Zahlen-Gewinnspiel");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
